fe8ce77ff98a8449bd0121ab7b5005651d752381
Group Dc Bots <98206257+GroupDcBots@users.noreply.github.com>
2022-01-26 14:48:18 +0530

    Start

---
+++ Main/Plugins/start.py
@@ -0,0 +1,63 @@
+#Github.com/Vasusen-code
+
+import os
+from .. import bot
+from telethon import events, Button, TelegramClient
+
+from pyrogram import idle
+from main.plugins.main import Bot, userbot
+
+st = "Oii Buddy 🤡 __Send me Link of any message to clone it here, For private channel message, Send invite link first.__\n\nSUPPORT: @groupdcbots\nDEV: @selfiebd"
+
+@bot.on(events.NewMessage(incoming=True, pattern="/start"))
+async def start(event):
+    await event.reply(f'{st}', 
+                      buttons=[
+                              [Button.inline("SET THUMB.", data="sett"),
+                               Button.inline("REM THUMB.", data="remt")]
+                              ])
+    try:
+        await Bot.start()
+        await userbot.start()
+        await idle()
+    except Exception as e:
+        if 'Client is already connected' in str(e):
+            pass
+        else:
+            return
+    
+@bot.on(events.callbackquery.CallbackQuery(data="sett"))
+async def sett(event):    
+    Drone = event.client                    
+    button = await event.get_message()
+    msg = await button.get_reply_message() 
+    await event.delete()
+    async with Drone.conversation(event.chat_id) as conv: 
+        xx = await conv.send_message("Send me any image for thumbnail as a `reply` to this message. @groupdc")
+        x = await conv.get_reply()
+        if not x.media:
+            xx.edit("No media found.")
+        mime = x.file.mime_type
+        if not 'png' in mime:
+            if not 'jpg' in mime:
+                if not 'jpeg' in mime:
+                    return await xx.edit("No image found. @groupdcbots")
+        await xx.delete()
+        t = await event.client.send_message(event.chat_id, 'Trying.')
+        path = await event.client.download_media(x.media)
+        if os.path.exists(f'{event.sender_id}.jpg'):
+            os.remove(f'{event.sender_id}.jpg')
+        os.rename(path, f'./{event.sender_id}.jpg')
+        await t.edit("Temporary thumbnail saved! @groupdcbots")
+        
+@bot.on(events.callbackquery.CallbackQuery(data="remt"))
+async def remt(event):  
+    Drone = event.client            
+    await event.edit('Trying.')
+    try:
+        os.remove(f'{event.sender_id}.jpg')
+        await event.edit('Removed!')
+    except Exception:
+        await event.edit("No thumbnail saved. @groupdcbots")                        
+    
+    
